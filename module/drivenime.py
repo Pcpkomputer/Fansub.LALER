a8a1b9d6c015dca7e89dadfbbfe94b9a8fe2a2ce
Padang Perwira Yudha <terrificsubs@gmail.com>
2018-05-12 17:50:32 +0700

    twice bess (2)

--- module/drivenime.py
+++ module/drivenime.py
@@ -10,18 +10,24 @@ def drivenime(x):
     req = Request(url_awal, headers={"user-Agent" : "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"})
     res = urlopen(req)
     html = BeautifulSoup(res, "html.parser")
-    bagilagi=re.findall(r"href=\"(http(s|)://bagilagi.com/[^\"]+)\"",str(html))
+    bagilagi=re.findall(r"href=\"(http(s|)://bagilagi.com/[^\"]+)\" rel=\"nofollow\">[^p]+p]",str(html))
     #indexbl=len(bagilagi)-2
-    reso=['720p', '480p']
+    reso=re.findall(r"nofollow\">[^<]+[^p]/a>([^<]+)<p>",str(html))
     gd=[]
-    for x in range(2):
+    resoz=[]
+    for x in range(3):
+        a=str(reso[x]).replace(r"\xa0",r"")
+        a=re.sub(r"\s","",a)
+        resoz.append(a)
+        
+    for x in range(3):
         a=str(bagilagi[x]).replace(r"('","")
         a=a.replace(r"', '')","")
         kodetogel=re.search(r"\?id=(.*)",str(a))
         html=bagilagi_bypass(kodetogel.group(1))
         drive=re.search(r"changeLink\(\){var a=\'([^']+)\';window",str(html))
         gd.append(drive.group(1))
-    return(reso,gd)
+    print(resoz,gd)
         
     #if indexbl>0:
     #    for x in range(2,len(bagilagi)):
